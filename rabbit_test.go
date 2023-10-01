89b4be89a6c792581bb19471ffceb41ec6b51d38
Michael Okoh <ordrizzy@gmail.com>
2023-10-01 18:47:04 +0100

    Test for empty `AppName`

--- rabbit_test.go
+++ rabbit_test.go
@@ -1,6 +1,7 @@
 package rabbit
 
 import (
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -14,10 +15,8 @@ func TestGeneratePublicProperty(t *testing.T) {
 	c := Config{Job: "testJob"}
 	generatedParams := c.generateProperty(params...)
 
-	// Expected serialized parameters
 	expectedParams := "s:9:\"paramName\";s:10:\"paramValue\";"
 
-	// Assertions
 	assert.Equal(t, expectedParams, generatedParams)
 }
 
@@ -29,10 +28,8 @@ func TestGenerateInvalidProperty(t *testing.T) {
 	c := Config{Job: "testJob"}
 	generatedParams := c.generateProperty(params...)
 
-	// Expected serialized parameters
 	expectedParams := "s:9:\"paramName\";s:10:\"paramValue\";"
 
-	// Assertions
 	assert.Equal(t, expectedParams, generatedParams)
 }
 
@@ -44,10 +41,8 @@ func TestGenerateProtectedProperty(t *testing.T) {
 	c := Config{Job: "testJob"}
 	generatedParams := c.generateProperty(params...)
 
-	// Expected serialized parameters
 	expectedParams := fmt.Sprintf("s:%d:\"\u0000*\u0000%s\";s:%d:\"%s\";", len("paramName")+3, "paramName", len("paramValue"), "paramValue")
 
-	// Assertions
 	assert.Equal(t, expectedParams, generatedParams)
 }
 
@@ -59,29 +54,37 @@ func TestGeneratePrivateProperty(t *testing.T) {
 	c := Config{Job: "testJob"}
 	generatedParams := c.generateProperty(params...)
 
-	// Expected serialized parameters
 	expectedParams := fmt.Sprintf("s:%d:\"\u0000%s\u0000%s\";s:%d:\"%s\";", len(c.Job)+2, c.Job, "paramName", len("paramValue"), "paramValue")
 
-	// Assertions
 	assert.Equal(t, expectedParams, generatedParams)
 }
 
 func TestDelay(t *testing.T) {
 	c := Config{Delay: 5}
 
-	// Expected response
 	expected := fmt.Sprintf("i:%d;", c.Delay)
 
-	// Assertions
 	assert.Equal(t, expected, c.generateDelay())
 }
 
 func TestNoDelay(t *testing.T) {
 	c := Config{Delay: 0}
 
-	// Expected response
 	expected := "N;"
 
-	// Assertions
 	assert.Equal(t, expected, c.generateDelay())
+}
+
+func TestEmptyAppName(t *testing.T) {
+	params := []Param{
+		{Type: "protected", Name: "paramName", Value: "paramValue"},
+	}
+
+	c := Config{}
+
+	generatedParams := c.Dispatch(params...)
+
+	expectedParams := errors.New("AppName is required in config")
+
+	assert.Equal(t, expectedParams, generatedParams)
 }
